90541ed0937435291142de5e34c28ca042877014
VijayMasimukku <vijayps4223@gmail.com>
2022-01-25 12:01:27 +0530

    create object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -2,6 +2,7 @@ function CreateObject(arr) {
     // Write your code here
     var newObject={}
     for (var i=0;i<arr.length;i+=2){
+        
         newObject[arr[i]]=arr[i+1]
     }
     return newObject
